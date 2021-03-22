963fe5574315fb740929e6f849a424809d26eac0
harshaathnere <harshaathnere@gmail.com>
2021-03-22 15:18:07 +0530

    updation

--- src/component/ChatFeed.js
+++ src/component/ChatFeed.js
@@ -2,15 +2,36 @@ import MessageForum from './MessageForum';
 import MyMessage from './MyMessage';
  import TheirMessage from './TheirMessage.js';
 const ChatFeed = (props) => {
-    const {chats, activechat, userName, messages } = props;
-    const chat = chats && chats[activechat];
+    //console.log(props)
+    const {chats, activeChat, userName, messages } = props;
+    const chat = chats && chats[activeChat];
 
-    const renderMessage = () => {
+    const renderMessages = () => {
         const keys = Object.keys(messages);
-        console.log(messages)
+        console.log(keys);
+         return keys.map((key,index)=>{
+             const message = messages[key];
+             const lastMessage = index ===0 ? null : keys[index-1];
+             const isMyMessage = userName === message.sender.userName;
+              return(
+                  <div key ={`msg_${index}`} style={{width: "100%"}}>
+                  <div className="message-block">
+                  {
+                      isMyMessage ? <MyMessage/> : <TheirMessage/>
+                  }
+
+                  </div>
+                  <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px',marginLeft:isMyMessage ? '0px' : '68px'}}>
+                  read-receipts
+                  </div>
+
+                  </div>
+              )
+
+         })
     }
-    renderMessage();
- console.log(chat,  userName, messages);
+    renderMessages();
+
   return(
       <div>
           ChatFeed
